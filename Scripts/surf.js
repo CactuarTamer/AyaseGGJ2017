c25674faea7f994d69b5a5250c6a43ba3d73534a
CactuarTamer <cactuar.tamer@gmail.com>
2017-01-21 01:48:24 +0900

    #1 tweaked margins

--- Scripts/surf.js
+++ Scripts/surf.js
@@ -1,10 +1,14 @@
-
+var fps = 30; //frames per second for animating things
+var winW = window.innerWidth;
+var winH = window.innerHeight;
+var canvasW = winW - 50;
+var canvasH = 600;
 
 function init(){
 	canvas = document.getElementById("main");
 	context = canvas.getContext("2d");
 
-	context.canvas.width = 800;
-	context.canvas.height = 600;
+	context.canvas.width = canvasW;
+	context.canvas.height = canvasH;
 }
 
